e082e09e6a4397b3e7da84b32ef0b4817c0d652e
Jihyeon0804 <cam080421@naver.com>
2025-09-01 12:46:51 +0900

    fix

--- src/main/java/com/order/order/common/auth/SecurityConfig.java
+++ src/main/java/com/order/order/common/auth/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfiguration(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.cam080421.shop"));
         // 모든 HTTP(get, post 등) 메서드 허용 (허용 도메인 명시)
         configuration.setAllowedMethods(Arrays.asList("*"));
         // 모든 헤더 요소(Authorization 등) 허용
